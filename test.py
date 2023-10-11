79f1f96514a2dacbe3e77c3e44d0de2ad75e8a15
butcd <butcdmitrii@gmail.com>
2023-10-11 22:30:59 +0300

    added flip so the screen refreshes and its now drawing a circle

--- test.py
+++ test.py
@@ -3,9 +3,13 @@ import sys
 pygame.init()
 
 screen = pygame.display.set_mode((1200, 800))
+#r = pygame.Rect(50, 50, 100, 200)
+#pygame.draw.rect(screen, (255, 0, 0), r, 0)
 
+pygame.draw.circle(screen, (255,0,0), (200,200), 50, width=5)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        pygame.display.flip()
