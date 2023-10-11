dcc8c01097c9a843e61af520c903797947f33e9a
butcd <butcdmitrii@gmail.com>
2023-10-11 23:03:13 +0300

    changed rectangle color and added a line

--- test.py
+++ test.py
@@ -4,9 +4,11 @@ pygame.init()
 
 screen = pygame.display.set_mode((1200, 800))
 r = pygame.Rect(575, 375, 50, 50)
-pygame.draw.rect(screen, (255, 0, 0), r, 0)
+pygame.draw.rect(screen, (0, 255, 0), r, 0)
 
 pygame.draw.circle(screen, (255,0,0), (600,400), 100, width=5)
+
+pygame.draw.line(screen, (255,0,0), (400,400), (800,400), width=5)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
