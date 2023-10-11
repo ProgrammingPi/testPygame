96bf766ffbebe068e3765c9edc40ede92562f8a8
dmitrii Butc <132802346+ProgrammingPi@users.noreply.github.com>
2023-10-11 23:03:53 +0300

    changed rectangle color (#2)

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ pygame.init()
 
 screen = pygame.display.set_mode((1200, 800))
 r = pygame.Rect(575, 375, 50, 50)
-pygame.draw.rect(screen, (255, 0, 0), r, 0)
+pygame.draw.rect(screen, (255, 0, 255), r, 0)
 
 pygame.draw.circle(screen, (255,0,0), (600,400), 100, width=5)
 while True:
