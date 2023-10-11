c768c98e4f3fe7c69cf6bc049830668f6aafac55
dmitrii Butc <132802346+ProgrammingPi@users.noreply.github.com>
2023-10-11 22:43:25 +0300

    Task1 drawing circle in pygame (#1)
    * added flip so the screen refreshes and its now drawing a circle
    
    * changed the circle size and its in the middle, added a cube in the middle and changed color

--- test.py
+++ test.py
@@ -3,9 +3,13 @@ import sys
 pygame.init()
 
 screen = pygame.display.set_mode((1200, 800))
+r = pygame.Rect(575, 375, 50, 50)
+pygame.draw.rect(screen, (255, 0, 0), r, 0)
 
+pygame.draw.circle(screen, (255,0,0), (600,400), 100, width=5)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        pygame.display.flip()
